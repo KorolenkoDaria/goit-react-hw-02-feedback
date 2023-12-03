558c3bf42230f5929b7f6b5aab0794aa0180d54f
KorolenkoDaria <admem75213@gmail.com>
2023-12-03 14:33:59 +0200

    bugfix

--- src/components/App/App.styled.jsx
+++ src/components/App/App.styled.jsx
@@ -4,7 +4,5 @@ export const Wrapper = styled.div`
     width: 1280px;
     height: 100vh;
     margin: 0 auto;
-    border-right: 1px solid #828282;
-    border-left: 1px solid #828282;
     padding: 60px 24px;
 `
